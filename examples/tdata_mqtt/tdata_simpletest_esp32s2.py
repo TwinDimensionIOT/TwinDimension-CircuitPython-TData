47245bed18d3f89a08fe33d6affe2028ac185077
Luis Pichio <lpichio@tesacom.net>
2023-08-25 14:30:40 -0300

    +T>Data Gateway API

---
+++ examples/tdata_mqtt/tdata_simpletest_esp32s2.py
@@ -0,0 +1,130 @@
+# SPDX-FileCopyrightText: 2023 Luis Pichio, for TwinDimension
+# SPDX-License-Identifier: MIT
+import time
+from random import randint
+
+import ssl
+import socketpool
+import wifi
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+from tdata.tdata import TData_MQTT_Gateway
+import json
+import microcontroller
+import gc
+
+### WiFi ###
+
+# Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
+# "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
+# source control.
+# pylint: disable=no-name-in-module,wrong-import-order
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+print("Connecting to %s" % secrets["ssid"])
+wifi.radio.connect(secrets["ssid"], secrets["password"])
+print("Connected to %s!" % secrets["ssid"])
+
+
+# Define callback functions which will be called when certain events happen.
+# pylint: disable=unused-argument
+def connected(client):
+    # Connected function will be called when the client is connected to TDATA.
+    print("Connected to T>DATA!")
+
+
+def subscribe(client, userdata, topic, granted_qos):
+    # This method is called when the client subscribes to a new topic.
+    print("Subscribed to {0} with QOS level {1}".format(topic, granted_qos))
+
+
+def unsubscribe(client, userdata, topic, pid):
+    # This method is called when the client unsubscribes from a topic.
+    print("Unsubscribed from {0} with PID {1}".format(topic, pid))
+
+# pylint: disable=unused-argument
+
+
+def disconnected(client):
+    # Disconnected function will be called when the client disconnects.
+    print("Disconnected from TDATA!")
+
+# pylint: disable=unused-argument
+
+
+def message(client, feed_id, payload):
+    # Message function will be called when a subscribed topic has a new value.
+    # The feed_id parameter identifies the feed, and the payload parameter has
+    # the new value.
+    print("Feed {0} received new value: {1}".format(feed_id, payload))
+
+
+# Create a socket pool
+pool = socketpool.SocketPool(wifi.radio)
+
+# Initialize a new MQTT Client object
+mqtt_client = MQTT.MQTT(
+    broker="tdata.tesacom.net",
+    port=1883,
+    username=secrets["tdata_gateway_token"],
+    password="",
+    socket_pool=pool,
+    #    ssl_context=ssl.create_default_context(),
+)
+
+# Initialize an TData Client
+tdata = TData_MQTT_Gateway(mqtt_client)
+
+# Connect the callback methods defined above to TDATA
+tdata.on_connect = connected
+tdata.on_disconnect = disconnected
+tdata.on_subscribe = subscribe
+tdata.on_unsubscribe = unsubscribe
+tdata.on_message = message
+
+# Connect to TDATA
+print("Connecting to TDATA...")
+tdata.connect()
+
+device_name = "EP(%02X:%02X:%02X:%02X:%02X:%02X)" % tuple(wifi.radio.mac_address)
+
+# Below is an example of manually publishing a new  value to TDATA.
+lastTelemetry = 0
+lastAttributes = 0
+print("Publishing telemetry every 10 seconds and attributes every 60...")
+while True:
+    # Explicitly pump the message loop.
+    tdata.loop()
+    # Send a new message every 10 seconds.
+    if (time.monotonic() - lastTelemetry) >= 10:
+        telemetry = {
+            device_name: [
+                {
+                    "cpu.temperature": microcontroller.cpu.temperature,
+                    "cpu.voltage": microcontroller.cpu.voltage,
+                    "gc.mem_alloc": gc.mem_alloc(),
+                    "gc.mem_free": gc.mem_free(),
+                    "wifi.radio.ap_info.rssi": wifi.radio.ap_info.rssi,
+                }
+            ]
+        }
+        print("Publishing telemetry: ", telemetry)
+        tdata.publish("telemetry", telemetry)
+        lastTelemetry = time.monotonic()
+
+    if (time.monotonic() - lastAttributes) >= 60:
+        attributes = {
+            device_name: {
+                "radio.ipv4_address": wifi.radio.ipv4_address,
+                "cpu.reset_reason": microcontroller.cpu.reset_reason,
+                "cpu.frequency": microcontroller.cpu.frequency,
+                "wifi.radio.ap_info.channel": wifi.radio.ap_info.channel,
+                "wifi.radio.ap_info.ssid": wifi.radio.ap_info.ssid,
+            }
+        }
+        print("Publishing attributes: ", attributes)
+        tdata.publish("attributes", attributes)
+        lastAttributes = time.monotonic()
